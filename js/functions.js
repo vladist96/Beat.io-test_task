1800f931cf3055e28e6f0c30956e5c52fb0b97ff
vladist96 <mr.frost096@gmail.com>
2019-04-07 03:32:00 +0300

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+    const submitForm = () => {
+        let name = document.getElementById("name").value;
+        let sms = document.getElementById("text").value;
+        console.log(" Name : " + name + " \n Email : " + sms);
+    };
